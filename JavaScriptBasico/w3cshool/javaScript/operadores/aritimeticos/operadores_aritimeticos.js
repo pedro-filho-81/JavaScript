eab92b14d94bf1b959b0db41ad2f1440bf89ee04
PedroFilho <psfilho81@hotmail.com>
2023-01-29 12:20:25 -0300

    Create operadores_aritimeticos.js

---
+++ JavaScriptBasico/w3cshool/javaScript/operadores/aritimeticos/operadores_aritimeticos.js
@@ -0,0 +1,14 @@
+/*
+A marca HTML <script>
+A marca HTML é usada para definir um script do lado do cliente (JavaScript).<script>
+
+O elemento contém instruções de script ou aponta para um arquivo de script externo através do atributo:
+<script src="">
+
+Os usos comuns do JavaScript são manipulação de imagens, validação de formulários e alterações dinâmicas de conteúdo.
+
+Para selecionar um elemento HTML, o J-avaScript geralmente usa o método.document.getElementById()
+*/
+      
+
+alert("Olá, JavaScript, testando script externo.");
