37e88f3e4b03ae6aca569270eaca8a7911326acd
PedroFilho <psfilho81@hotmail.com>
2023-01-29 12:19:29 -0300

    Atualização

--- JavaScriptBasico/palavra_chave_const/palavra_chave_const.js
+++
@@ -1,5 +0,0 @@
-
-window.alert("Uma variável const não pode ser reatribuída:");
-const PI = 3.141592653589793;
-PI = 3.14;      // This will give an error
-PI = PI + 10;   // This will also give an error
