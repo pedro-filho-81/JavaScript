86d647ca5f191e8ac805f03296b39d105e392bac
PedroFilho <psfilho81@hotmail.com>
2023-01-31 18:16:15 -0300

    Create oprd_maior_que.js

---
+++ JavaScriptBasico/w3cshool/javaScript/operadores/comparacao/oprd_maior_que.js
@@ -0,0 +1,21 @@
+/*
+   <h2>O operador maior que <mark> > </mark></h2>
+
+   <p>O operador maior que <mark> > </mark> compara os valores para saber <br>se o valor1 é maior que o valor2? se for então,<br>retorna verdadeiro; caso contrário retorna falso.</p>
+
+   <p>Atribua 5 a variável x, e exiba <strong>true ou false</strong> <br>com base nos valores da comparação:</p>
+
+   <p><strong>let x <mark>=</mark> 5</strong>; cria uma variável do tipo inteiro.</p>
+
+   <pre>
+   &lt;body> <code>// aqui html</code>
+      &lt;!--O parágrafo com o (id="demo") vai ser alterado pelo JavaScript-->
+      &lt;p id="<mark>demo</mark>">&lt;/p>
+
+   &lt;script> <code>// aqui javascript dentro do HTml</code>
+*/
+   let x = 5; // declara variável do tipo inteiro
+
+   // aqui a comparação é entre dois valores diferentes e do mesmo tipo.
+   // 5 é maior que 8
+   document.getElementById("demo").innerHTML = ("x é maior que 8?" + (x > 8) ); // resultado falso
